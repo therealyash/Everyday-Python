6bb15eb6cb067174a03dc240852ed1ffe59c5420
therealyash <yashraj1416@gmail.com>
2024-08-11 18:25:35 +0530

    Decision Control Instructions 1

---
+++ Conditional_Expressions.py
@@ -0,0 +1,20 @@
+# Condition Expressions
+'''
+Syntax
+<expr1> if <conditional expression> else <expr2>
+'''
+
+# Example 1
+a = 20
+b = 20
+print(True if a > b else False)
+
+# Example 2
+weight = 65
+msg = 'Obese' if weight > 80 else 'Hefty' if weight > 60 else 'Normal'
+print(msg)
+
+# Example 3
+cric_score = 0
+msg = 'Duck' if cric_score == 0 else 'Half Century!' if cric_score == 50 else 'Century!' if cric_score == 100 else "Near to Half Century!" if cric_score > 0 and cric_score < 51 else "Near to Century!" if cric_score < 101 and cric_score > 50 else 'Invalid Score'
+print(msg)
---
+++ Operators.py
@@ -0,0 +1,17 @@
+# Operators in Python Practice
+
+# or (operators which need 2 operands are called binary operator)
+a = 20
+b = 30
+c = 60 or a >= 20 or b > a or 80
+print(c)
+
+# not (operators which need one operand are called unary operator)
+d = int(input("Enter 0 or 1: "))
+d = not d
+print(d)
+
+
+
+
+
