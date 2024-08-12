bdb9571afa5d01d97fdad25f13feb1e85c671c64
therealyash <yashraj1416@gmail.com>
2024-08-12 23:47:40 +0530

    Decision Control Instructions 2

--- Conditional_Expressions.py
+++ Conditional_Expressions.py
@@ -18,3 +18,11 @@ print(msg)
 cric_score = 0
 msg = 'Duck' if cric_score == 0 else 'Half Century!' if cric_score == 50 else 'Century!' if cric_score == 100 else "Near to Half Century!" if cric_score > 0 and cric_score < 51 else "Near to Century!" if cric_score < 101 and cric_score > 50 else 'Invalid Score'
 print(msg)
+
+# Example 4
+c = 25
+d = 35
+op = all((a > 0, b > 0))
+print(op)
+op = any((a < 0, b < 0))
+print(op)
